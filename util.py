0e7a5d5cb00cef8d90e1476e0d7d37e76e696de5
rzamzam <126548575+rzamzam@users.noreply.github.com>
2023-12-12 17:54:55 +0700

    Update util.py

--- util.py
+++ util.py
@@ -1,33 +1,4 @@
-import base64
-
-import streamlit as st
-from PIL import ImageOps, Image
-import numpy as np
-
-
-def set_background(image_file):
-    """
-    This function sets the background of a Streamlit app to an image specified by the given image file.
-
-    Parameters:
-        image_file (str): The path to the image file to be used as the background.
-
-    Returns:
-        None
-    """
-    with open(image_file, "rb") as f:
-        img_data = f.read()
-    b64_encoded = base64.b64encode(img_data).decode()
-    style = f"""
-        <style>
-        .stApp {{
-            background-image: url(data:image/png;base64,{b64_encoded});
-            background-size: cover;
-        }}
-        </style>
-    """
-    st.markdown(style, unsafe_allow_html=True)
-
+from tensorflow.keras.preprocessing import image as keras_image
 
 def classify(image, model, class_names):
     """
@@ -42,23 +13,25 @@ def classify(image, model, class_names):
     Returns:
         A tuple of the predicted class name and the confidence score for that prediction.
     """
-    # convert image to (224, 224)
+    # Convert image to grayscale (single-channel)
+    image = ImageOps.grayscale(image)
+
+    # Resize image to (224, 224)
     image = ImageOps.fit(image, (224, 224), Image.Resampling.LANCZOS)
 
-    # convert image to numpy array
+    # Convert image to numpy array
     image_array = np.asarray(image)
 
-    # normalize image
+    # Normalize image
     normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
 
-    # set model input
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-    data[0] = normalized_image_array
+    # Set model input
+    data = np.ndarray(shape=(1, 224, 224, 1), dtype=np.float32)
+    data[0] = normalized_image_array[:, :, np.newaxis]  # Add a channel dimension
 
-    # make prediction
+    # Make prediction
     prediction = model.predict(data)
-    # index = np.argmax(prediction)
-    index = 0 if prediction[0][0] > 0.95 else 1
+    index = np.argmax(prediction)
     class_name = class_names[index]
     confidence_score = prediction[0][index]
 
