fe17ac8bd8fb18377ec025eef183e81d809bf071
rzamzam <126548575+rzamzam@users.noreply.github.com>
2023-12-12 18:27:42 +0700

    Update util.py

--- util.py
+++ util.py
@@ -7,37 +7,27 @@ import numpy as np
 from tensorflow.keras.preprocessing import image as keras_image
 
 def classify(image, model, class_names):
-    """
-    This function takes an image, a model, and a list of class names and returns the predicted class and confidence
-    score of the image.
-
-    Parameters:
-        image (PIL.Image.Image): An image to be classified.
-        model (tensorflow.keras.Model): A trained machine learning model for image classification.
-        class_names (list): A list of class names corresponding to the classes that the model can predict.
-
-    Returns:
-        A tuple of the predicted class name and the confidence score for that prediction.
-    """
     # Convert image to grayscale (single-channel)
     image = ImageOps.grayscale(image)
 
     # Resize image to (224, 224)
     image = ImageOps.fit(image, (224, 224), Image.Resampling.LANCZOS)
 
-    # convert image to numpy array
+    # Convert image to numpy array
     image_array = np.asarray(image)
 
-    # normalize image
+    # Convert grayscale image to RGB by replicating the single channel to all three
+    image_array = np.stack((image_array,) * 3, axis=-1)
+
+    # Normalize image
     normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
 
-    # set model input
+    # Set model input
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
     data[0] = normalized_image_array
 
-    # make prediction
+    # Make prediction
     prediction = model.predict(data)
-    # index = np.argmax(prediction)
     index = 0 if prediction[0][0] > 0.95 else 1
     class_name = class_names[index]
     confidence_score = prediction[0][index]
