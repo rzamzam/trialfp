9c123c70447f2c55ed6605d69986b38d37f2f200
rzamzam <126548575+rzamzam@users.noreply.github.com>
2023-12-12 19:03:17 +0700

    Update util.py

--- util.py
+++ util.py
@@ -1,40 +1,10 @@
 import base64
 
-# util.py
-
-import numpy as np
-from PIL import ImageOps, Image
-import base64
 import streamlit as st
+from PIL import ImageOps, Image
+import numpy as np
 
-def classify(image, model, class_names):
-    # Resize image to (224, 224)
-    image = ImageOps.fit(image, (224, 224), Image.Resampling.LANCZOS)
 
-    # Convert image to numpy array
-    image_array = np.asarray(image)
-
-    # Convert image to grayscale
-    gray_image_array = ImageOps.grayscale(image)
-
-    # Convert grayscale image to numpy array
-    gray_image_array = np.asarray(gray_image_array)
-
-    # Normalize grayscale image
-    normalized_gray_image_array = (gray_image_array / 127.5) - 1
-
-    # Set model input
-    data = np.ndarray(shape=(1, 224, 224, 1), dtype=np.float32)
-    data[0, :, :, 0] = normalized_gray_image_array
-
-    # Make prediction
-    prediction = model.predict(data)
-    index = np.argmax(prediction)
-    class_name = class_names[index]
-    confidence_score = prediction[0][index]
-
-    return class_name, confidence_score
-    
 def set_background(image_file):
     """
     This function sets the background of a Streamlit app to an image specified by the given image file.
@@ -57,4 +27,39 @@ def set_background(image_file):
         </style>
     """
     st.markdown(style, unsafe_allow_html=True)
-   
+
+
+def classify(image, model, class_names):
+    """
+    This function takes an image, a model, and a list of class names and returns the predicted class and confidence
+    score of the image.
+
+    Parameters:
+        image (PIL.Image.Image): An image to be classified.
+        model (tensorflow.keras.Model): A trained machine learning model for image classification.
+        class_names (list): A list of class names corresponding to the classes that the model can predict.
+
+    Returns:
+        A tuple of the predicted class name and the confidence score for that prediction.
+    """
+    # convert image to (224, 224)
+    image = ImageOps.fit(image, (224, 224), Image.Resampling.LANCZOS)
+
+    # convert image to numpy array
+    image_array = np.asarray(image)
+
+    # normalize image
+    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+
+    # set model input
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    data[0] = normalized_image_array
+
+    # make prediction
+    prediction = model.predict(data)
+    # index = np.argmax(prediction)
+    index = 0 if prediction[0][0] > 0.95 else 1
+    class_name = class_names[index]
+    confidence_score = prediction[0][index]
+
+    return class_name, confidence_score
